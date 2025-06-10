e1115c165f41d386e01e0f1cac2400d3fd2238ce
Stefano <153748666+stexgang@users.noreply.github.com>
2025-06-10 16:46:21 +0200

    Add files via upload

--- hw1.sql
+++ hw1.sql
@@ -23,3 +23,16 @@ CREATE TABLE IF NOT EXISTS carrello (
 )Engine = INNODB;
 
 
+CREATE TABLE IF NOT EXISTS prodotti (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    prezzo DECIMAL(10,2) NOT NULL,
+    immagine VARCHAR(255) NOT NULL
+);
+INSERT INTO prodotti (nome, prezzo, immagine) VALUES
+('iPhone 15 Pro', 1299.00, 'iphone16.jpg'),
+('MacBook Air M2', 1499.00, 'mac.jpg'),
+('iPad Pro 11"', 1099.00, 'images/ipadpro11.png'),
+('Apple Watch Series 9', 499.00, 'watch.jpg'),
+('AirPods Pro', 299.00, 'images/airpodspro.png'),
+('Apple TV 4K', 199.00, 'images/appletv4k.png');
